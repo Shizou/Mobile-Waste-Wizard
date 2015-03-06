9f137a33f40a33693a10afd6be1d8e3e5593ff4c
William G. <will.grand.wg@gmail.com>
2015-03-06 17:17:12 -0500

    Added java doc and more
    Added java doc and made the computation for edit distance more clear.

--- MobileWasteWizard/src/com/example/mobilewastewizard/Stream.java
+++ MobileWasteWizard/src/com/example/mobilewastewizard/Stream.java
@@ -5,16 +5,25 @@ public class Stream {
 	public Stream() {
 		
 	}
+	/**Returns whether true if the ith character in string
+	 * A is the same as the jth character in string B.
+	 * @param A first string
+	 * @param B second string
+	 * @param i ith character in A
+	 * @param j jth character in B*/
 	public int diff(String A, String B,int i,int j){
 		return A.charAt(i) == B.charAt(j) ? 0:1;
 	}
-	public int edit_distance(String A,String B){
-	    A = " " + A;
-	    B = " " + B;
-		int N = A.length(), M = B.length(),inf = 1000000;
+	/**Returns the edit-distance between two strings
+	 * Time complexity is O(NM) where N is the length of
+	 * first string and M is the length of the second string
+	 * @param A first string
+	 * @param B second string  */
+	public int editDistance(String A,String B){
+		int N = A.length()-1, M = B.length()-1,inf = 1000000;
 		int [][]dp = new int[N][M];
-		for(int i = 0;i < N;i++)
-			for(int j = 0;j < M;j++)
+		for(int i = 0;i <= N;i++)
+			for(int j = 0;j <= M;j++)
 				dp[i][j] = inf;
 		for(int i = 0;i < N;i++){
 			for(int j = 0;j < M;j++){
@@ -23,10 +32,10 @@ public class Stream {
 				else if(j == 0)
 					dp[i][j] = i;
 				else
-					dp[i][j] = Math.min(dp[i-1][j-1]+diff(A,B,i,j), Math.min(dp[i-1][j]+1,dp[i][j-1]+1) );
+					dp[i][j] = Math.min(dp[i-1][j-1]+diff(A,B,i-1,j-1), Math.min(dp[i-1][j]+1,dp[i][j-1]+1) );
 			}
 		}
-		return dp[N-1][M-1];
+		return dp[N][M];
 	}
 	
 }
