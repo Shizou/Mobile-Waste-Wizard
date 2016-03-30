1eee68d2e4b059ba15c4e84ef06c9e5e852a7a6f
William Granados <will.grand.wg@gmail.com>
2016-03-30 17:36:30 -0400

    I've done some major clean coding on the database and textfilehandler

--- MobileWasteWizard/src/com/example/mobilewastewizard/Database.java
+++ MobileWasteWizard/src/com/example/mobilewastewizard/Database.java
@@ -1,5 +1,5 @@
 /** Name: Burhan Qadri, Justin Li, William Granados
- *  Date: 25/03/15
+ *  Date: 30/03/16
  *  Purpose: Handles queries to the database
  * */
 package com.example.mobilewastewizard;
@@ -10,7 +10,8 @@ import java.util.List;
 
 import android.content.Context;
 
-public class Database {
+public class Database 
+{
 	
 	private final int CATEGORIES_SIZE = 10, LIMIT = 20;
 	private String categoriesName[];
@@ -19,12 +20,17 @@ public class Database {
 	private TextFileHandler textFileSupport;
 
 	/**Initializes the database
-	 * @param cxt the context from which this class acquires privileges for the assets folder (in this case main activity)*/
-	public Database(Context cxt) {
+	 * @param cxt the context from which this
+	 *  class acquires privileges for the assets folder (in this case 
+	 *  main activity)*/
+	public Database(Context cxt) 
+	{
+		// Initialize miscellaneous stuff
 		this.textFileSupport = new TextFileHandler(cxt);
 		this.categoriesName = new String[this.CATEGORIES_SIZE];
 		this.categories = textFileSupport.getCategories();
 		this.totalList = textFileSupport.getTotalList();
+	    // Assign each index the corresponding terminology	
 		this.categoriesName[0] = "Blue Bin";
 		this.categoriesName[1] = "Transfer Station or Waste Depot";
 		this.categoriesName[2] = "E-Waste";
@@ -43,42 +49,85 @@ public class Database {
 	 * @param A first string
 	 * @param B second string
 	 * @param i ith character in A
-	 * @param j jth character in B*/
-	public int diff(String A, String B,int i,int j){
-		return A.charAt(i) == B.charAt(j) ? 0:1;
+	 * @param j jth character in B
+	 * */
+	public int diff(String A, String B,int i,int j)
+	{
+		// check for equivalence
+		if(A.charAt(i) == B.charAt(j))
+		{
+			return 0;
+		}
+		else
+		{
+			return 1;
+		}
 	}
+	
 	/**Returns the edit-distance between two strings
 	 * Time complexity is O(NM) where N is the length of
 	 * first string and M is the length of the second string
 	 * @param A first string
-	 * @param B second string  */
-	public int editDistance(String A,String B){
-		int N = A.length(), M = B.length(),inf = 1000000;
+	 * @param B second string 
+	 *  */
+	public int editDistance(String A,String B)
+	{
+		int N = A.length();
+		int M = B.length();
+		int inf = 1000000;
 		int [][]dp = new int[N][M];
+	    // arbitrarily set the cost for each to be infinity	
 		for(int i = 0;i < N;i++)
+		{
 			for(int j = 0;j < M;j++)
+			{
 				dp[i][j] = inf;
-		for(int i = 0;i < N;i++){
-			for(int j = 0;j < M;j++){
+			}
+		}
+		// Now we calculate the cost or "edit distance" associated with the
+		// sub strings of String A and String B. We then build off this by 
+		// doing some dynamic programming logic
+		for(int i = 0;i < N;i++)
+		{
+			for(int j = 0;j < M;j++)
+			{
+				// 
 				if(i == 0)
+				{
 					dp[i][j] = j;
+				}
+			    // 	
 				else if(j == 0)
+				{
 					dp[i][j] = i;
+				}
+			    // 	
 				else
-					dp[i][j] = Math.min(dp[i-1][j-1]+diff(A,B,i-1,j-1), Math.min(dp[i-1][j]+1,dp[i][j-1]+1) );
+				{
+					dp[i][j] = Math.min(Math.min(dp[i-1][j]+1, dp[i][j-1]+1),
+							            dp[i][j]);
+					dp[i][j] = Math.min(dp[i-1][j-1]+diff(A,B,i-1,j-1),
+										dp[i][j]);
+				}
 			}
 		}
 		return dp[N-1][M-1];
 	}
+	
 	/**Checks whether a pattern is present in a text field
 	 * Time complexity is O(NM) where N is the length of the 
 	 * text field and M is the query
 	 * @param text text to search pattern from
-	 * @param pattern pattern to search for*/
-	public boolean search(String text,String pattern){
+	 * @param pattern pattern to search for
+	 * */
+	public boolean search(String text,String pattern)
+	{
 		// TODO use KMP algorithm for faster run times
-		for(int i = 0;i < text.length();i++){
-			for(int j = 0;j < pattern.length();j++){
+	    // in this case it's still O(N^2)	
+		for(int i = 0;i < text.length();i++)
+		{
+			for(int j = 0;j < pattern.length();j++)
+			{
 				if(text.charAt(i+j)!=pattern.charAt(j))break;
 				if(j == pattern.length()-1)return true;
 			}
@@ -87,45 +136,61 @@ public class Database {
 	}
 		
 	/**Looks through the entire database looking for an exact match
-	 * Uses binary search so find the query
-	 * @param query string to be queried*/
-	public String initialQuery(String query){
-		for(int i = 0;i < this.CATEGORIES_SIZE;i++){
+	 * Uses binary search so find the query in O(logN)
+	 * @param query string to be queried
+	 * */
+	public String initialQuery(String query)
+	{
+		for(int i = 0;i < this.CATEGORIES_SIZE;i++)
+		{
 			int index = Collections.binarySearch(categories.get(i), query);
 			if(index >= 0)
+			{
 				return categoriesName[i];
+			}
 		}
 		return null;
 	}
+
 	/**Looks through all of the items in the database and returns
 	 * values which are most similar to the query. First checks for
 	 * values that contain the parts of the query then checks their
 	 * edit distance
-	 * @param query string to be queried*/
-	public List<String>secondaryQuery(String query){
+	 * @param query string to be queried
+	 * */
+	public List<String>secondaryQuery(String query)
+	{
 		List< String >list = new ArrayList< String >();
-		List< Pair<Integer,String> >list2 = new ArrayList< Pair<Integer,String> >();
+		List< Pair<Integer,String> >list2 = new ArrayList< 
+												Pair<Integer,String> >();
 		List<String>suggestions = new ArrayList<String>();
-		for(int i = 0;i < this.CATEGORIES_SIZE;i++){
-			for(int j = 0;j < this.categories.get(i).size();j++){
-				if(categories.get(i).get(j).contains(query)){
+		for(int i = 0;i < this.CATEGORIES_SIZE;i++)
+		{
+			for(int j = 0;j < this.categories.get(i).size();j++)
+			{
+				if(categories.get(i).get(j).contains(query))
+				{
 					list.add(categories.get(i).get(j));
 					break;
 				}						
 			}
 		}
 		Collections.sort(list);
-		for(int i = 0;i < this.LIMIT && i < list.size();i++){
-			list2.add(new Pair<Integer,String>(editDistance(list.get(i),query), list.get(i)));
+		for(int i = 0;i < this.LIMIT && i < list.size();i++)
+		{
+			list2.add(new Pair<Integer,String>(
+					  editDistance(list.get(i),query), list.get(i)));
 		}
 		Collections.sort(list2);
-		for(int i = 0;i < this.LIMIT && i < list2.size();i++){
+		for(int i = 0;i < this.LIMIT && i < list2.size();i++)
+		{
 			suggestions.add(list2.get(i).second);
 		}
 		return suggestions.size() == 0 ? null:suggestions;
 	}
-	
-	public List<String> getTotalList(){
+	/** Returns the a list containing all of the terms in the database*/
+	public List<String> getTotalList()
+	{
 		return this.totalList;
 	}
 }
--- MobileWasteWizard/src/com/example/mobilewastewizard/TextFileHandler.java
+++ MobileWasteWizard/src/com/example/mobilewastewizard/TextFileHandler.java
@@ -1,6 +1,6 @@
 /** Name: Burhan Qadri, Justin Li, William Granados
- *  Date: 25/03/15
- *  Purpose: Handles the reading in from database
+ *  Date: 30/03/16
+ *  Purpose: Handles reading in information to the database
  * */
 
 package com.example.mobilewastewizard;
@@ -9,45 +9,80 @@ import java.util.*;
 import android.content.*;
 import android.support.v7.app.ActionBarActivity;
 
-public class TextFileHandler extends ActionBarActivity  {
+public class TextFileHandler extends ActionBarActivity  
+{
 	
 	private final int CATEGORIES_SIZE = 10;
 	private List<List<String>> categories = new ArrayList<List<String>>();
 	private List<String>totalList = new ArrayList<String>();
 	
-	/**Initializes categories by reading them from file.
-	 * @param cxt the context from which this class acquires privileges for the assets folder (in this case main activity)*/
-	public TextFileHandler(Context cxt){
-		for(int i = 0;i < this.CATEGORIES_SIZE;i++)// creates the new categories
+	/** Initializes categories then reads them from the file
+	 * @param cxt the context from which this class acquires privileges 
+	 * for the assets folder (in this case main activity)
+	 * */
+	public TextFileHandler(Context cxt)
+	{
+		// Initialize the categories
+		for(int i = 0;i < this.CATEGORIES_SIZE;i++)
+		{
 			this.categories.add(new ArrayList<String>());
-		this.openCategoryFile(cxt,"categories/Blue.txt", 	categories.get(0));
-		this.openCategoryFile(cxt,"categories/BTTSWD.txt", 	categories.get(1));
-		this.openCategoryFile(cxt,"categories/EWaste.txt", 	categories.get(2));
-		this.openCategoryFile(cxt,"categories/Green.txt", 	categories.get(3));
-		this.openCategoryFile(cxt,"categories/Grey.txt", 	categories.get(4));
-		this.openCategoryFile(cxt,"categories/HHW.txt", 	categories.get(5));
-		this.openCategoryFile(cxt,"categories/OW.txt", 		categories.get(6));
-		this.openCategoryFile(cxt,"categories/PW.txt", 		categories.get(7));
-		this.openCategoryFile(cxt,"categories/SM.txt", 		categories.get(8));
-		this.openCategoryFile(cxt,"categories/YW.txt", 		categories.get(9));
+		}
+	    // Now populate each categories with the terms available in the text
+		// files. Just for reference, here are what each terminology each index
+		// represents
+		
+		// 0) Blue Bin
+		// 1) Bring to Transfer Station or Waste Depot
+		// 2) E-Waste
+		// 3) Green Bin
+		// 4) Grey Bin - Garbage
+		// 5) Household Hazardous Waste
+		// 6) Over-sized Waste
+		// 7) Prohibited Waste
+		// 8) Scrap Metal
+		// 9) Yard Waste
+		
+		this.openCategoryFile(cxt,"categories/Blue.txt", categories.get(0));
+		this.openCategoryFile(cxt,"categories/BTTSWD.txt", categories.get(1));
+		this.openCategoryFile(cxt,"categories/EWaste.txt", categories.get(2));
+		this.openCategoryFile(cxt,"categories/Green.txt", categories.get(3));
+		this.openCategoryFile(cxt,"categories/Grey.txt", categories.get(4));
+		this.openCategoryFile(cxt,"categories/HHW.txt", categories.get(5));
+		this.openCategoryFile(cxt,"categories/OW.txt", categories.get(6));
+		this.openCategoryFile(cxt,"categories/PW.txt", categories.get(7));
+		this.openCategoryFile(cxt,"categories/SM.txt", categories.get(8));
+		this.openCategoryFile(cxt,"categories/YW.txt", categories.get(9));
+		// in the OpenCategoryFile, all of the terms are also added to a total
+		// list so we are simply sorting the entries for further usage
 		Collections.sort(totalList);
 	}
 
 	/**Opens a file and returns contents from it into an array list
 	 * @param file file path for the file
-	 * @param list list for information to be stored in*/
-	public void openCategoryFile(Context cxt,String file,List<String>list){
-		try {
-			InputStream  	  is   = cxt.getAssets().open(file);
+	 * @param list list for information to be stored in
+	 * */
+	public void openCategoryFile(Context cxt, String file,List<String>list)
+	{
+		// Lets try and open the the text file that was handed to us
+		// then we'll append each term to the respective list object, and
+		// to a global list object
+		try 
+		{
+			InputStream is = cxt.getAssets().open(file);
 			InputStreamReader isr  = new InputStreamReader(is);
-			BufferedReader 	  br   = new BufferedReader(isr);
-			String 			  line = br.readLine();
-			while(line!=null){
+			BufferedReader br = new BufferedReader(isr);
+			String line = br.readLine();
+		    // Read to EOF	
+			while(line != null)
+			{
 				list.add(line);
 				totalList.add(line);
 				line = br.readLine();
 			}
-		} catch (IOException e) {
+		} 
+	    // Something went terribly! Likely just that the file wasn't there	
+		catch (IOException e) 
+		{
 			System.out.println("File not found:" + file);
 			e.printStackTrace();
 		}
@@ -56,6 +91,7 @@ public class TextFileHandler extends ActionBarActivity  {
 	public List<List<String>> getCategories(){
 		return this.categories;
 	}
+	
 	/**Returns the total list which contains all the items*/
 	public List<String>getTotalList(){
 		return this.totalList;
