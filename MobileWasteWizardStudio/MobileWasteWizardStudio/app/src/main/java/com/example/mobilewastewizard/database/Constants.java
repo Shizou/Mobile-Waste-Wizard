93336713a1c98c23018030a30ae548e7de2f96b1
William Granados <me@wgma00.me>
2016-12-04 16:01:31 -0500

    refactored code by placing classes into appropriate packages and also moved some variables to the constants

---
+++ MobileWasteWizardStudio/MobileWasteWizardStudio/app/src/main/java/com/example/mobilewastewizard/database/Constants.java
@@ -0,0 +1,45 @@
+package com.example.mobilewastewizard.database;
+
+public class Constants {
+
+  /** Number of categories that the city of Toronto supports.*/
+  public static final int CATEGORIES_SIZE = 10;
+  /** Maximum number of similar entries to be returned when checking 
+   * for similar words.*/
+  public static final int LIMIT = 20;
+
+  public static String[] categoriesName   = {"Blue Bin", "Transfer Station or Waste Depot",
+                                            "E-Waste", "Green Bin", "Grey Bin - Garbage",
+                                            "Household Hazardous Waste", "Oversized Waste",
+                                            "Prohibited Waste", "Scrap Metal", "Yard Waste"};
+
+  /** Blue Bin. */
+  public static final int BLUE_BIN = 0;
+  /** Bring to Transfer Station or Waste Depot. */
+  public static final int BTTWSD = 1;
+  /** E-Waste.*/
+  public static final int E_WASTE = 2;
+  /** Green Bin.*/
+  public static final int GREEN_BIN = 3;
+  /** Grey Bin - Garbage.*/
+  public static final int GREY_BIN = 4;
+  /** Household Hazardous Waste.*/
+  public static final int HHW = 5;
+  /** Over-sized Waste.*/
+  public static final int OW = 6;
+  /** Prohibited Waste.*/
+  public static final int PW = 7;
+  /** Scrap Metal.*/
+  public static final int SM = 8;
+  /** Yard Waste.*/
+  public static final int YW = 9;
+
+
+  /** State for image capture.*/
+  public static final int REQ_CAPTURE_IMAGE = 0;
+  /** State for voice recognition. */
+  public static final int REQ_VOICE = 1;
+  /** State for image capture.*/
+  public static final int REQUEST_IMAGE_CAPTURE = 2;
+
+}
