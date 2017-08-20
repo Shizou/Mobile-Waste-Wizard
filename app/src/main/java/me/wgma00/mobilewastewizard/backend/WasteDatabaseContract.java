36c117737da9bd6b44cd3e3c8506ceb750056d92
William Granados <me@wgma00.me>
2017-08-20 03:47:45 -0400

    added items to help define SQLite schema

---
+++ app/src/main/java/me/wgma00/mobilewastewizard/backend/WasteDatabaseContract.java
@@ -0,0 +1,35 @@
+package me.wgma00.mobilewastewizard.backend;
+
+import android.provider.BaseColumns;
+
+
+/** Defines the basic database schema for SQLite
+ */
+public class WasteDatabaseContract {
+    public static final  int    DATABASE_VERSION   = 1;
+    public static final  String DATABASE_NAME      = "waste_database.db";
+    private static final String TEXT_TYPE          = " TEXT";
+    private static final String COMMA_SEP          = ",";
+
+    // To prevent someone from accidentally instantiating the contract class,
+    // give it an empty constructor.
+    private WasteDatabaseContract() {}
+
+    public static abstract class Table1 implements BaseColumns {
+        public static final String TABLE_NAME       = "Waste";
+        public static final String COLUMN_NAME_COL1 = "title";
+        public static final String COLUMN_NAME_COL2 = "alt_words";
+        public static final String COLUMN_NAME_COL3 = "desc_id";
+        public static final String COLUMN_NAME_COL4 = "description";
+
+
+        public static final String CREATE_TABLE = "CREATE TABLE " +
+                TABLE_NAME + " (" +
+                _ID + " INTEGER PRIMARY KEY," +
+                COLUMN_NAME_COL1 + TEXT_TYPE + COMMA_SEP +
+                COLUMN_NAME_COL2 + TEXT_TYPE + COMMA_SEP +
+                COLUMN_NAME_COL3 + TEXT_TYPE + COMMA_SEP +
+                COLUMN_NAME_COL4 + TEXT_TYPE + " )";
+        public static final String DELETE_TABLE = "DROP TABLE IF EXISTS " + TABLE_NAME;
+    }
+}
