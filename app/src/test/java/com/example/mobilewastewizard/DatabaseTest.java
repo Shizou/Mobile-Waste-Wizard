e5d80683068d56a67a4b8a5cfa0f406b5b262bf7
William Granados <me@wgma00.me>
2017-10-13 01:11:35 -0400

    fixed testNG tests

---
+++ app/src/test/java/com/example/mobilewastewizard/DatabaseTest.java
@@ -0,0 +1,21 @@
+package com.example.mobilewastewizard;
+
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
+
+/**
+ * Created by william on 13/10/17.
+ */
+public class DatabaseTest {
+    @Test
+    public void testDiff() throws Exception {
+
+    }
+
+    @Test
+    public void testEditDistance() throws Exception {
+
+    }
+
+}
